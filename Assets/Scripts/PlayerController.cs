778b0d18b7db57331fbc1b0dac607530f90e2ccc
zeppzeynep <zeynepaydin2003.55@gmail.com>
2026-05-08 15:13:55 +0300

    particle systems added
    -  splatted dirt
    -  explossion smoke

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,14 @@ using UnityEngine.InputSystem.Interactions;
 
 public class PlayerController : MonoBehaviour
 {
+    public ParticleSystem dirt;
+    public ParticleSystem puff;
+    private Rigidbody playerRb;
+    private Animator playerAnim;
     public bool gameOver;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
-    private Rigidbody playerRb;
-    private Animator playerAnim;
 
 
     void Start()
@@ -25,6 +27,7 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirt.Stop();
         }
     }
 
@@ -32,12 +35,16 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
+            dirt.Play();
+
         }else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            puff.Play();
+            dirt.Stop();
         }
     }
 }
